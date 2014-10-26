8e8e9de2bfc0b9f2a776eb138dd055978af5fb1c
Mike Dillon <mike.dillon@synctree.com>
2014-10-25 20:53:24 -0700

    Allow nil fallback to disable fallback behavior

--- lib/rack/conneg.rb
+++ lib/rack/conneg.rb
@@ -101,7 +101,7 @@ module Rack #:nodoc:#
     # What MIME type should be used as a fallback if negotiation fails? Defaults to 'text/html'
     # since that's what's used to deliver most error message content.
     def fallback
-      find_mime_type(@opts[:fallback])
+      find_mime_type(@opts[:fallback]) unless @opts[:fallback].nil?
     end
     
     # Specifies a route prefix or Regexp that should be ignored by the content negotiator. Use
